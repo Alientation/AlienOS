5e59f84e5b726724ab2e4bf691fd55b714b8a21c
Bill Ma <mcpirate1760@gmail.com>
2025-12-24 10:42:23 -0600

    KMalloc Magic number

--- src/kernel/kmalloc.c
+++ src/kernel/kmalloc.c
@@ -12,6 +12,9 @@
 /* Memory block header flag bits in metadata. */
 #define KMALLOC_ALLOC_BIT 0b0001
 
+/* Magic number stored in the padding. */
+#define KMALLOC_MAGIC 0xF00BA700
+
 #define KMALLOC_ALIGN(x, align) ((((x) + (align) - 1) / (align)) * (align))
 
 extern uint32_t kheap_begin;
@@ -21,10 +24,10 @@ static uint32_t kheap_max_end;
 
 struct KMStats
 {
-    uint32_t allocation_cnt;            /* How many times the kmalloc() or krealloc() is called. */
-    uint32_t allocation_bytes;          /* How many bytes was requested in total. */
+    uint32_t allocation_cnt;            /* How many times the kmalloc(), kcalloc(), or krealloc() is called. */
+    size_t allocation_bytes;            /* How many bytes was requested in total. */
     uint32_t free_cnt;                  /* How many times kfree() is called. */
-    uint32_t free_bytes;                /* How many bytes was freed in total. */
+    size_t free_bytes;                  /* How many bytes was freed in total. */
 } kmalloc_stats = {};
 
 
@@ -78,23 +81,23 @@ static bool internal_read_multibootinfo (const multiboot_info_t *const mbinfo)
     return true;
 }
 
-static size_t kmalloc_getsize (const km_block_header_t * const block)
+static inline size_t kmalloc_getsize (const km_block_header_t * const block)
 {
     return block->metadata & ~(0b1111);
 }
 
 /* Assumption is size is 16 byte aligned and does not include the header size. */
-static void kmalloc_setsize (km_block_header_t * const block, const size_t size)
+static inline void kmalloc_setsize (km_block_header_t * const block, const size_t size)
 {
     block->metadata = (block->metadata & (0b1111)) | size;
 }
 
-static bool kmalloc_isalloc (const km_block_header_t * const block)
+static inline bool kmalloc_isalloc (const km_block_header_t * const block)
 {
     return block->metadata & KMALLOC_ALLOC_BIT;
 }
 
-static void kmalloc_setalloc (km_block_header_t * const block, const bool alloc)
+static inline void kmalloc_setalloc (km_block_header_t * const block, const bool alloc)
 {
     if (alloc)
     {
@@ -106,8 +109,18 @@ static void kmalloc_setalloc (km_block_header_t * const block, const bool alloc)
     }
 }
 
+static inline bool kmalloc_checkmagic (const km_block_header_t * const block)
+{
+    return block->pad[0] == KMALLOC_MAGIC;
+}
+
+static inline void kmalloc_setmagic (km_block_header_t * const block)
+{
+    block->pad[0] = KMALLOC_MAGIC;
+}
+
 /* Initializes the header of the memory block. Does not insert into the free list. */
-static void kmalloc_initblock (km_block_header_t * const block, const size_t size)
+static inline void kmalloc_initblock (km_block_header_t * const block, const size_t size)
 {
     kmalloc_setsize (block, size);
     kmalloc_setalloc (block, false);
@@ -141,26 +154,41 @@ void kmalloc_init (const multiboot_info_t * const mbinfo)
     kheap_end = kheap_begin;
     kmalloc_extend (KMALLOC_HEAP_INIT_SIZE);
     io_serial_printf (COMPort_1, "Kernal Heap: [%x, %x] (MAX %x)\n", kheap_begin, kheap_end, kheap_max_end);
-
-
 }
 
 void *kmalloc (const size_t size)
 {
+
+    kmalloc_stats.allocation_cnt++;
+    kmalloc_stats.allocation_bytes += size;
+
+    // TODO:
     return NULL;
 }
 
 void *kcalloc (const size_t nelems, const size_t elemsize)
 {
-    return NULL;
+    const size_t bytes = nelems * elemsize;
+    uint8_t *mem = (uint8_t *) kmalloc (bytes);
+    for (size_t i = 0; i < bytes; i++)
+    {
+        mem[i] = 0;
+    }
+
+    return (void *) mem;
 }
 
 void *krealloc (void * const ptr, const size_t size)
 {
+    // TODO:
+
     return NULL;
 }
 
 void kfree (void * const ptr)
 {
+    kmalloc_stats.free_cnt++;
+    kmalloc_stats.free_bytes += kmalloc_getsize (ptr);
 
+    // TODO:
 }
