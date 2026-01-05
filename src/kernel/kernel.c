26dc53439b5a25df84ba4c5d0c29bab8c5db4292
Bill Ma <mcpirate1760@gmail.com>
2026-01-05 15:27:17 -0600

    Move old io test code

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -23,34 +23,6 @@
 #error "Needs to be compiled with a ix86-elf compiler"
 #endif
 
-static void test_io (void)
-{
-	io_serial_set_loopback (COMPort_1, true);
-	io_serial_outb (COMPort_1, 0xAB);
-
-	uint8_t data = 0;
-	if (!io_serial_nextinb (COMPort_1, &data))
-	{
-		terminal_writestr ("FAILED SPIN ITERATIONS\n\n");
-	}
-	else if (data != 0xAB)
-	{
-		terminal_writestr ("FAILED DATA MATCH\n\n");
-	}
-
-	io_serial_set_loopback (COMPort_1, false);
-
-	io_serial_outstr (COMPort_1, "abc\n");
-	io_serial_outint (COMPort_1, 206);
-	io_serial_outstr (COMPort_1, "\n");
-	io_serial_outint (COMPort_1, -206);
-	io_serial_outstr (COMPort_1, "\n");
-	io_serial_outbool (COMPort_1, false);
-	io_serial_outstr (COMPort_1, "\n");
-	io_serial_outbool (COMPort_1, true);
-	io_serial_outstr (COMPort_1, "\n");
-}
-
 void kernel_main(const unsigned int magic, const multiboot_info_t * const mbinfo)
 {
 	static bool init = false;
--- src/tests/io_test.c
+++ src/tests/io_test.c
@@ -1,6 +1,35 @@
 #include "alienos/tests/unit_tests.h"
 #include "alienos/io/io.h"
 
+
+// static void test_io (void)
+// {
+// 	io_serial_set_loopback (COMPort_1, true);
+// 	io_serial_outb (COMPort_1, 0xAB);
+
+// 	uint8_t data = 0;
+// 	if (!io_serial_nextinb (COMPort_1, &data))
+// 	{
+// 		terminal_writestr ("FAILED SPIN ITERATIONS\n\n");
+// 	}
+// 	else if (data != 0xAB)
+// 	{
+// 		terminal_writestr ("FAILED DATA MATCH\n\n");
+// 	}
+
+// 	io_serial_set_loopback (COMPort_1, false);
+
+// 	io_serial_outstr (COMPort_1, "abc\n");
+// 	io_serial_outint (COMPort_1, 206);
+// 	io_serial_outstr (COMPort_1, "\n");
+// 	io_serial_outint (COMPort_1, -206);
+// 	io_serial_outstr (COMPort_1, "\n");
+// 	io_serial_outbool (COMPort_1, false);
+// 	io_serial_outstr (COMPort_1, "\n");
+// 	io_serial_outbool (COMPort_1, true);
+// 	io_serial_outstr (COMPort_1, "\n");
+// }
+
 void io_test (struct UnitTestsResult * const result)
 {
 
