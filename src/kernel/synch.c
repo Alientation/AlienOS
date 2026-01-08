71cc5c641439a54621d66952c7cf2206a4148e6d
Bill Ma <mcpirate1760@gmail.com>
2026-01-08 10:58:40 -0600

    Comments in synch.c

--- src/kernel/synch.c
+++ src/kernel/synch.c
@@ -5,10 +5,11 @@
 
 #include <stddef.h>
 
-/* Must be synchronized externally. */
+/* Inserts at the the end of the doubly linked list. Must be synchronized externally. */
 static void wait_queue_append (tlistnode_t ** const head, tlistnode_t ** const tail,
                                tlistnode_t * const thread)
 {
+    /* List is empty. */
     if (!*head)
     {
         *head = thread;
@@ -18,23 +19,34 @@ static void wait_queue_append (tlistnode_t ** const head, tlistnode_t ** const t
         return;
     }
 
+    /* Set new tail. */
     (*tail)->next = thread;
     thread->next = NULL;
     thread->prev = *tail;
     *tail = thread;
 }
 
-/* Must be synchronized externally. */
+/* Removes from the beginning of doubly linked list. Must be synchronized externally. */
 static thread_t *wait_queue_popfront (tlistnode_t ** const head, tlistnode_t ** const tail)
 {
     kernel_assert (*head, "wait_queue_popfront(): Expected nonempty lists");
+    kernel_assert (!(*head)->prev, "wait_queue_popfront(): Expected head to have no previous node");
 
     tlistnode_t * const unblocked = *head;
+
+    /* Move head over. */
     *head = (*head)->next;
     if (!*head)
     {
+        /* List is empty. */
         *tail = NULL;
     }
+    else
+    {
+        /* Remove backwards edge. */
+        (*head)->prev = NULL;
+    }
+
     unblocked->next = NULL;
     return unblocked->thread;
 }
@@ -75,6 +87,7 @@ bool semaphore_try_down (semaphore_t * const sem)
     const bool interrupts = interrupt_disable ();
     bool success = false;
 
+    /* If atleast one resource is available take it. If not don't block. */
     if (sem->count > 0)
     {
         sem->count--;
@@ -113,6 +126,8 @@ void mutex_init (mutex_t * const mutex)
 void mutex_acquire (mutex_t * const mutex)
 {
     const bool interrupts = interrupt_disable ();
+
+    /* Already holds the lock, add to recursion count so we release appropriately. */
     if (mutex->holder == current_thread)
     {
         mutex->recursion_count++;
@@ -133,12 +148,14 @@ void mutex_acquire (mutex_t * const mutex)
 
 bool mutex_try_acquire (mutex_t * const mutex)
 {
+    /* We already hold it. */
     if (mutex->holder == current_thread)
     {
         mutex->recursion_count++;
         return true;
     }
 
+    /* Try to acquire it. */
     if (semaphore_try_down (&mutex->sem))
     {
         mutex->holder = current_thread;
@@ -154,9 +171,11 @@ void mutex_release (mutex_t * const mutex)
     const bool interrupts = interrupt_disable ();
     kernel_assert (mutex->holder == current_thread, "mutex_release(): Owner thread must release the lock");
 
+    /* In case we reacquired the same lock several times. */
     mutex->recursion_count--;
     kernel_assert (mutex->recursion_count != ~0UL, "mutex_release(): Overflow");
 
+    /* If we have released the same number of times we have acquired, fully release lock. */
     if (mutex->recursion_count == 0)
     {
         mutex->holder = NULL;
@@ -176,12 +195,14 @@ void condvar_wait (condvar_t * const cond, mutex_t * const mutex)
 {
     const bool interrupts = interrupt_disable ();
 
+    /* Wait on a signal, releasing the lock. */
     current_thread->status = ThreadStatus_Blocked;
     current_thread->blocked_on = cond;
     current_thread->blocker_type = BlockerType_CondVar;
     wait_queue_append (&cond->wait_queue_head, &cond->wait_queue_tail, &current_thread->local_list);
     mutex_release (mutex);
 
+    /* Give up execution until we are signaled. */
     thread_yield ();
 
     interrupt_restore (interrupts);
@@ -192,6 +213,7 @@ void condvar_signal (condvar_t * const cond)
 {
     const bool interrupts = interrupt_disable ();
 
+    /* Unblock the first in the queue. */
     if (cond->wait_queue_head)
     {
         thread_t * const wake_thread = wait_queue_popfront (&cond->wait_queue_head, &cond->wait_queue_tail);
@@ -205,6 +227,7 @@ void condvar_broadcast (condvar_t * const cond)
 {
     const bool interrupts = interrupt_disable ();
 
+    /* Unlock all in the queue. */
     while (cond->wait_queue_head)
     {
         thread_t * const wake_thread = wait_queue_popfront (&cond->wait_queue_head, &cond->wait_queue_tail);
